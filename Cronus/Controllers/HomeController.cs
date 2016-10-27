f97887448fcc5f2f0a475aad33b7c8ce4761afc1
Brandon Maroney <bmaroney17@outlook.com>
2016-10-27 17:06:59 -0400

    //////Needs to be revised -- will move models eventually to repository. created the modesl for the homepage, with the function to pull between dates.

--- Cronus/Controllers/HomeController.cs
+++ Cronus/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,87 @@ namespace Cronus.Controllers
 {
     public class HomeController : Controller
     {
+        private CronusDatabaseEntities db = new CronusDatabaseEntities();
+        HomeViewModel myModel = new HomeViewModel();
+
+
         public ActionResult Index()
         {
-            return View();
+
+            myModel.Projects = db.projects.ToList();
+            myModel.Activities = db.activities.ToList();
+            myModel.HoursWorked = db.hoursworkeds.ToList();
+            return View(myModel);
+
+        }
+
+        public ActionResult Monthly()
+        {
+            //MonthlyViewModel myModel = new MonthlyViewModel();
+            return View(myModel);
+        }
+
+        [HttpGet]
+        public ViewResult GetBetweenDates(string employeeID)
+        {
+            HomeViewModel homeModel = new HomeViewModel();
+
+            //DateTime previousWeek = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 7);
+            DateTime startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 6);
+            DateTime endDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek);
+
+
+            //when getting previous week, must have the start of the time period be the previous monday.
+            //For example lets say its sunday and you click previous week, youll be getting the incorrect dates.
+
+            //if 1 week ago was tuesday, set start to 1 day previous, so start date startes on a monday, ends on a sunday.
+            if (startDate.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek);
+            }
+            //if 1 week ago was wednesday, set start to 2 day previous, so start date startes on a monday, ends on a sunday.
+            //repeat for each day.
+            else if (startDate.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 1);
+            }
+            else if (startDate.DayOfWeek == DayOfWeek.Thursday)
+            {
+                startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 2);
+            }
+            else if (startDate.DayOfWeek == DayOfWeek.Friday)
+            {
+                startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 3);
+            }
+            else if (startDate.DayOfWeek == DayOfWeek.Saturday)
+            {
+                startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 4);
+            }
+            else if (startDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                startDate = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - 5);
+            }
+
+
+            homeModel.HoursWorked = db.hoursworkeds
+                                                .Where(n => n.date >= startDate)
+                                                .Where(n => n.date <= endDate)
+                                                .Where(n => n.TimePeriod_Employee_employeeID == employeeID);
+
+            return View(homeModel);
+        }
+
+
+        //going to be working on saving the hours listed into the DB.
+        [HttpPost]
+        public ActionResult SaveHours(HomeViewModel model)
+        {
+            //pass through a model filled with information, push that information to the corresponding DB table.
+            hoursworked hoursWorked = new hoursworked();
+            hoursWorked.comments = model.hrsWorked.comments;
+            hoursWorked.date = model.hrsWorked.date;
+            //finish that later
+            return View(model);
         }
 
         public ActionResult About()
---
+++ DatabaseEntities/Partial/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseEntities
+{
+    public class HomeViewModel
+    {
+            public IEnumerable<project> Projects { get; set; }
+            public IEnumerable<activity> Activities { get; set; }
+            public IEnumerable<favorite> Favorites { get; set; }
+
+            //Working on pulling hours/activities based on week and employee
+            public IEnumerable<hoursworked> HoursWorked { get; set; }
+            public hoursworked hrsWorked { get; set; }
+         
+    }
+}
